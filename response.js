61b81f28a749bf180fa33deacb2227afa1620172
Donna Ann Issac <donna.ann1209@gmail.com>
2017-10-30 14:09:56 +0530

    Set content-type header

--- response.js
+++ response.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const status = {
   200: 'OK',
@@ -6,6 +7,24 @@ const status = {
   404: 'Not Found',
   500: 'Internal Server Error'
 }
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.gif': 'image/gif',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.svg': 'application/image/svg+xml'
+}
+
 class Response {
   constructor () {
     this.version = 'HTTP/1.1'
@@ -14,41 +33,52 @@ class Response {
     this.headers = {}
     this.body = ''
   }
-  generateResponse (request) {
-    console.log('In Response class' + request, this)
+  generateResponse (request, socket) {
+    console.log('In Response class')
     this.setHeaders(request)
-    if (request.url === null) {
-      this.setStatus(400)
-      return this.generateResStr()
-    }
-    let data = fs.readFileSync(request.url, 'utf-8')
-    if (data === undefined) {
-      this.setStatus(404)
-      return this.generateResStr()
-    }
-    this.body = data
-    return this.generateResStr()
+    console.log('Hey' + request.url)
+    fs.readFile(request.url, 'utf-8', (err, data) => {
+      if (err) {
+        this.setStatus(404)
+        this.writeToSocket(this.generateResStr(), socket)
+        return
+      } else if (data === undefined) {
+        this.setStatus(400)
+        this.writeToSocket(this.generateResStr(), socket)
+        return
+      }
+      this.body = data
+      this.writeToSocket(this.generateResStr(), socket)
+    })
   }
   setHeaders (request) {
-    let headers = {}
-    headers['Content-Type'] = request.headers['Content-Type']
-    headers['Date'] = new Date()
-    this.headers = headers
+    this.getContentType(request)
+    this.headers['Date'] = new Date()
   }
   generateResStr () {
     let str = ''
     str += this.version + ' ' + this.statusCode + ' ' + this.statusMessage + '\n'
     for (let i in this.headers) {
-      console.log('Check' + i, this.headers[i])
       if (this.headers.hasOwnProperty(i)) str += i + ': ' + this.headers[i] + '\n'
     }
     str += '\n' + this.body
+    console.log('Oohhh' + str)
     return str
   }
   setStatus (code) {
     this.statusCode = code
     this.statusMessage = status[code]
     this.body = this.statusCode + ' ' + this.statusMessage
+  }
+  getContentType (request) {
+    let ext = path.extname(request.url)
+    this.headers['Content-Type'] = mimeTypes[ext]
+  }
+  writeToSocket (str, socket) {
+    socket.write(str, 'utf-8', () => {
+      console.log('Write complete')
+      socket.end()
+    })
   }
 }
 
