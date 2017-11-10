eac2cd015d710f66eb467612c9f97c4e4b2743d8
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-10 08:57:15 +0530

    Add routes and handlers and method handler

--- tcp-server.js
+++ tcp-server.js
@@ -1,74 +1,89 @@
 const net = require('net')
 const Request = require('./request')
 const Response = require('./response')
-const port = 9000
+const parseRequest = require('./parseRequest.js')
+const logger = require('./logger')
+const staticFileHandler = require('./staticFileHandler.js')
 
-let server = net.createServer((socket) => {
-  console.log('New client connection made')
-  console.log(socket.remoteAddress + ':' + socket.remotePort)
-  let reqStr = ''
-  socket.on('data', (data) => {
-    reqStr += data
-    if (reqStr.includes('\r\n\r\n')) {
-      let arr = reqStr.split('\r\n\r\n')
-      let obj = headerParser(arr[0])
-      if (arr[1].length === 0 && obj.Method === 'GET') {
-        createReqAndRes(reqStr, socket)
-        reqStr = ''
-      } else {
-        if (parseInt(obj['Content-Length']) === arr[1].length) {
-          createReqAndRes(reqStr, socket)
+const routes = {
+  GET: {},
+  POST: {}
+}
+
+const handlers = []
+
+function startServer (port) {
+  let server = net.createServer((socket) => {
+    console.log('New client connection made')
+    console.log(socket.remoteAddress + ':' + socket.remotePort)
+    let reqStr = ''
+    socket.on('data', (data) => {
+      reqStr += data
+      if (reqStr.includes('\r\n\r\n')) {
+        let [header, body] = reqStr.split('\r\n\r\n')
+        let obj = parseRequest(header)
+        if (obj.Headers['Content-Length'] === undefined) {
+          let [request, response] = createReqAndRes(obj, socket)
+          next(request, response)
+          reqStr = ''
+        }
+        if (parseInt(obj.Headers['Content-Length']) === body.length) {
+          let [request, response] = createReqAndRes(obj, socket)
+          next(request, response)
           reqStr = ''
         }
       }
-    }
+    })
+    socket.on('end', () => {
+      console.log('End event')
+    })
+    socket.on('close', () => {
+      console.log('Connection from ' + socket.remoteAddress + ' closed')
+    })
+    socket.on('error', err => {
+      console.log(err)
+    })
   })
-  socket.on('end', () => {
-    console.log('End event')
+  server.listen(port, () => {
+    console.log('Listening on port: ' + port)
   })
-  socket.on('close', () => {
-    console.log('Connection from ' + socket.remoteAddress + ' closed')
+  server.on('error', (err) => {
+    throw err
   })
-  socket.on('error', err => {
-    console.log(err)
-  })
-})
-
-server.listen(port, () => {
-  console.log('Listening on port: ' + port)
-})
-
-server.on('error', (err) => {
-  throw err
-})
-
-function headerParser (headerStr) {
-  let arr = headerStr.split('\r\n')
-  let headerObj = {}
-  let reqLine = arr.shift().split(' ')
-  headerObj['Method'] = reqLine[0]
-  headerObj['Path'] = reqLine[1]
-  headerObj['Version'] = reqLine[2]
-  arr.forEach((element) => {
-    let header = element.split(':')
-    headerObj[header[0]] = header[1]
-  })
-  return headerObj
 }
 
-function foo (reqStr, socket) {
-
-}
-
-function next (req, res, socket) {
-  if (req.handlers.length === 0) socket.end()
-  let handler = req.handlers.shift()
-  handler(req, res, socket, next)
-}
-
-function createReqAndRes (reqStr, socket) {
-  let request = new Request(reqStr)
-  console.log(request)
+function createReqAndRes (reqObj, socket) {
+  let request = new Request(reqObj, handlers)
+  request['socket'] = socket
   let response = new Response(request)
-  next(request, response, socket)
+  return [request, response]
+}
+
+function next (req, res) {
+  if (req.handlers.length === 0) requestHandler(req, res)
+  let handler = req.handlers.shift()
+  handler(req, res, next)
+}
+
+const requestHandler = (req, res) => {
+  if (routes[req.method].hasOwnProperty(req.url)) routes[req.method][req.url](req, res)
+  else {
+    res.setStatus(404)
+    res.body = res.statusCode + ' ' + res.statusMessage
+    res.send()
+  }
+}
+
+function addRoutes (method, route, callback) {
+  routes[method][route] = callback
+}
+
+function addHandler (handler) {
+  handlers.push(handler)
+}
+
+module.exports = {
+  startServer,
+  addHandler,
+  addRoutes
 }
