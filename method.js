00e27ec7fb77f4f91751c1eb11214642afa12069
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-10 09:06:14 +0530

    Remove method.js

--- method.js
+++
@@ -1,9 +0,0 @@
-const getHandler = require('./get-handler')
-const postHandler = require('./post-handler')
-
-const chooseMethod = (req, res, socket, next) => {
-  if (req.method === 'GET') getHandler(req, res, socket, next)
-  if (req.method === 'POST') postHandler(req, res, next)
-}
-
-module.exports = chooseMethod
