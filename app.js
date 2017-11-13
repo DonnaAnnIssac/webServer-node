9787d12f817c343c21f48135d34d96fa1c227cb6
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-13 12:05:47 +0530

    Test multiple static file handler instances

--- app.js
+++ app.js
@@ -6,47 +6,13 @@ const staticFileHandler = require('./middleware/staticFileHandler')
 server.startServer(9000)
 server.addHandler(logger)
 server.addHandler(staticFileHandler('./test'))
-server.addRoutes('GET', '/about', (req, res) => {
-  fs.readFile('./form-test/about.html', (err, data) => {
-    if (err) throw err
-    res.body = data
-    res.setContentType('./form-test/about.html')
-    res.send()
-  })
-})
-
-server.addRoutes('GET', '/data', (req, res) => {
-  fs.readFile('./form-test/data.html', (err, data) => {
-    if (err) throw err
-    res.body = data
-    res.setContentType('./form-test/data.html')
-    res.send()
-  })
-})
-
-server.addRoutes('GET', '/form', (req, res) => {
-  fs.readFile('./form-test/form.html', (err, data) => {
-    if (err) throw err
-    res.body = data
-    res.setContentType('./form-test/form.html')
-    res.send()
-  })
-})
+server.addHandler(staticFileHandler('./form-test'))
 
 server.addRoutes('GET', '/bg.jpg', (req, res) => {
   fs.readFile('./test/images/bg.jpg', (err, data) => {
     if (err) throw err
     res.body = data
     res.setContentType('./test/images/bg.jpg')
-    res.send()
-  })
-})
-
-server.addRoutes('GET', '/paint-it-black.mp3', (req, res) => {
-  fs.readFile('./form-test/paint-it-black.mp3', (err, data) => {
-    if (err) throw err
-    res.body = data
-    res.setContentType('./form-test/paint-it-black.mp3')
     res.send()
   })
 })
