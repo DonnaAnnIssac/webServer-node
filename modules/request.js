66a9912b85ab31e8489a4ebcddb7b6c400cef6d7
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-13 15:38:05 +0530

    Remove parsing logic out of request class

--- modules/request.js
+++ modules/request.js
@@ -1,17 +1,12 @@
 class Request {
   constructor (reqObj, handlers) {
     this.method = reqObj.Method
-    this.url = parseUrl(reqObj.Path)
+    this.url = reqObj.Path
     this.version = reqObj.Version
     this.headers = reqObj.Headers
     this.body = ''
     this.handlers = [...handlers]
   }
-}
-
-function parseUrl (url) {
-  let i = url.indexOf('?')
-  return (i !== -1) ? url.slice(0, i) : url
 }
 
 module.exports = Request
