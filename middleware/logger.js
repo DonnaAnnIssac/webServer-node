5d946b97043b44e462be5ac3446e002d9bb3c087
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-13 15:32:07 +0530

    Specify correct path

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const logger = (req, res, next) => {
   let data = ''
   data += req.headers['Host'] + ' ' + new Date() + ' ' + req.method + ' ' + req.url + ' ' + req.version + '\n'
-  fs.appendFile('./logs.txt', data, (err) => {
+  fs.appendFile('./middleware/logs.txt', data, (err) => {
     if (err) throw err
     console.log('Log file updated')
     next(req, res)
