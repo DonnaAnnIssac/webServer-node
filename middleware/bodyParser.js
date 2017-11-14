deec28be514962e92a555fcaa17efe4dda2eb171
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-14 12:25:04 +0530

    Add bodyParser as handler

--- middleware/bodyParser.js
+++ middleware/bodyParser.js
@@ -1,18 +1,20 @@
-const bodyParser = (req, body) => {
+const bodyParser = (req, res, next) => {
   if (req.headers['Content-Type'] === ' application/x-www-form-urlencoded') {
-    let values = body.split('&')
+    let values = req.body.split('&')
     let parsedBody = {}
     values.forEach((element) => {
       parsedBody[element.split('=')[0]] = element.split('=')[1].replace('+', ' ')
     })
     req.body = parsedBody
+    next(req, res)
   } else if (req.headers['Content-Type'] === ' application/json') {
-    let values = body.slice(1, body.length - 3).split(',')
+    let values = req.body.slice(1, req.body.length - 3).split(',')
     let parsedBody = {}
     values.forEach((element) => {
       parsedBody[element.split(':')[0].trim()] = element.split(':')[1].trim()
     })
     req.body = parsedBody
+    next(req, res)
   }
 }
 
