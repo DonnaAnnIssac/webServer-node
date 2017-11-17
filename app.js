fb237a96668438551f0ef73fa6b6a7aaae01cbcb
Donna Ann Issac <donna.ann1209@gmail.com>
2017-11-17 08:58:38 +0530

    Add test code for session handler

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const server = require('./server/tcp-server')
 const logger = require('./middleware/logger')
 const bodyParser = require('./middleware/bodyParser')
 const fs = require('fs')
+const sessionHandler = require('./middleware/sessionHandler')
 const staticFileHandler = require('./middleware/staticFileHandler')
 
 server.startServer(9000)
 server.addHandler(bodyParser)
+server.addHandler(sessionHandler.handleSession)
 server.addHandler(logger)
 server.addHandler((req, res, next) => {
   console.log('My own handler for request: ' + req.body)
@@ -13,15 +15,6 @@ server.addHandler((req, res, next) => {
 })
 server.addHandler(staticFileHandler('./test'))
 server.addHandler(staticFileHandler('./form-test'))
-server.addRoutes('GET', '/bg.jpg', (req, res) => {
-  fs.readFile('./test/images/bg.jpg', (err, data) => {
-    if (err) throw err
-    res.body = data
-    res.setHeaders()
-    res.setContentType('./test/images/bg.jpg')
-    res.send()
-  })
-})
 
 server.addRoutes('POST', '/data.html', (req, res) => {
   res.body = req.body['say'] + ' ' + req.body['to']
@@ -31,14 +24,36 @@ server.addRoutes('POST', '/data.html', (req, res) => {
 })
 
 server.addRoutes('GET', '/home', (req, res) => {
-  res.body = 'Home Page'
-  res.send()
+  res.redirect('./about')
 })
 
 server.addRoutes('POST', '/submit.cgi', (req, res) => {
-  fs.appendFile('./write.txt', req.files['secret'])
+  console.log(req.files['secret'].length)
+  fs.appendFile('./copy.png', req.files['secret'])
   res.body = 'File written'
   res.setHeaders()
-  res.setContentType('/submit.cgi')
+  res.setContentType('.png')
   res.send()
+})
+
+server.addRoutes('POST', '/validate', (req, res) => {
+  sessionHandler.addSession(req, req.body['UserName'])
+  if (sessionHandler.getSession(req) !== undefined) res.redirect('./welcome')
+  else res.redirect('./login')
+})
+
+server.addRoutes('GET', '/welcome', (req, res) => {
+  if (sessionHandler.getSession(req) !== undefined) res.redirect('./welcome')
+  else res.redirect('./login')
+})
+
+server.addRoutes('GET', '/display', (req, res) => {
+  if (sessionHandler.getSession(req) !== undefined) {
+    res.body = 'User Name: ' + req.body['UserName']
+  } else res.redirect('./login')
+})
+
+server.addRoutes('GET', '/logout', (req, res) => {
+  sessionHandler.deleteSession(req)
+  res.redirect('./login')
 })
